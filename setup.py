e4ba549632d988dae8736fade405105069d8dea9
Henry Hsu <43148403+henrykohl@users.noreply.github.com>
2025-07-13 00:08:03 +0800

    code fixed one more

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from typing import List
 
 HYPEN_E_DOT='-e .'
 
-def get_requiremet(file_path:str)->List[str]:
+def get_requirement(file_path:str)->List[str]:
     requirements = []
     with open(file_path) as f:
         requirements=f.readlines()
